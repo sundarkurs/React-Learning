bb03668c53406df689ef4e1b07e6986a7c1dee9c
Sundar Urs <sundarkurs@gmail.com>
2018-11-12 17:10:34 +0530

    counters implementation

--- react-cart/src/components/counter.jsx
+++ react-cart/src/components/counter.jsx
@@ -2,28 +2,66 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   styles = {
-    fontSize: 15,
+    fontSize: 14,
     fontWeight: "bold"
   };
 
   render() {
+    console.log(this.props);
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
+        <h4>Counter {this.props.objCounter.id}</h4>
+        {this.props.children}
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={() => this.props.onIncrement(this.props.objCounter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.objCounter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+
+        <div>{this.renderTags()}</div>
       </div>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes +=
+      this.props.objCounter.value === 0 ? "badge-warning" : "badge-primary";
+    return classes;
+  }
+
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.objCounter;
+    return value === 0 ? "Zero" : value;
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>There are no tags!</p>;
+    }
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
 }
 
